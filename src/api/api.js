d9c82f2c022b15c2d276eb880af6bfc9b05ad28f
jeniffersouzaa <jeniffer.souza@multisoftware.com.br>
2025-05-27 16:05:45 -0300

    mudado ambiente

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001',    //'https://api-conectalutas.onrender.com',
+  baseURL: 'https://api-conectalutas.onrender.com',    //'https://api-conectalutas.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
